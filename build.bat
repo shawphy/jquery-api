ae7ee24205f2fbb66ba88a7aa17fd995af2c9c56
Shawphy <shawphy@gmail.com>
2010-11-07 21:54:04 +0800

    翻译至2013，修改 dist 里的目录为 gh-page 分支。修改 build.bat 文件

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
-del dist /q /s
-md dist
+del dist\*.html /q /s
+del dist\*.js /q /s
+del dist\*.png /q /s
+del dist\*.css /q /s
+del dist\*.jpg /q /s
 xcopy file dist /s
 java -jar .\build\saxon9he.jar jQueryAPI.en_US.xml index.zh_CN.xsl>dist\index.html
 pause
