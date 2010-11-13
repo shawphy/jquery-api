67c3ff5c618a77c563d1cc4970503102eb8a623d
Shawphy <shawphy@gmail.com>
2010-11-14 00:18:52 +0800

    完成插件编写部分

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ del dist\*.js /q /s
 del dist\*.png /q /s
 del dist\*.css /q /s
 del dist\*.jpg /q /s
+del dist\*.gif /q /s
 xcopy file dist /s
 java -jar .\build\saxon9he.jar jQueryAPI.en_US.xml index.zh_CN.xsl>dist\index.html
 pause
